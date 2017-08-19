47d925940a8cb13cd073ba5f2ae80eef09df22c4
Rakey Drammeh <rdrammeh@gmail.com>
2017-08-19 17:14:48 -0400

    Create moves table to track player movement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817194322) do
+ActiveRecord::Schema.define(version: 20170819210945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20170817194322) do
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
+  create_table "moves", force: :cascade do |t|
+    t.integer "latitude", null: false
+    t.integer "longitude", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_moves_on_game_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "password_digest", null: false
     t.string "username", null: false
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 20170817194322) do
   end
 
   add_foreign_key "games", "users"
+  add_foreign_key "moves", "games"
 end
