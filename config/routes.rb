f3a65569b4855c81e69456f3104df42d6450886b
Rakey Drammeh <rdrammeh@gmail.com>
2017-08-19 20:17:49 -0400

    Add routes for game high scores

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "users#index"
-  get "/games/all", to: "games#all" 
+  get "/games/all", to: "games#all"
 end
