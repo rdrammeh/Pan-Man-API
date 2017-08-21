9775b9f5ffcbabcfa434ec971fc1585497ee4a14
Rakey Drammeh <rdrammeh@gmail.com>
2017-08-21 17:17:37 -0400

    Update sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(email: params[:login][:email])
+    if @user && @user.authenticate(params[:login][:password])
       session[:user_id] = @user.id
       render json: @user
     end
