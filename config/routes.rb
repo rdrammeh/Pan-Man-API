c4753e926ffec80c01950007f8079e8200ac8859
Sofia Forsell <email": null>
2017-08-21 12:01:29 -0400

    fixed userindex and duration method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "users#index"
+  root "scores#all"
+
   get "/users/new", to: "user#create"
   get "/users/:id", to: "users#show"
   get "/scores/:id", to: "scores#show"
