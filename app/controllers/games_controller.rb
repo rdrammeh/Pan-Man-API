167ffc625f10937e5facc556965e646f7e96c4a5
Rakey Drammeh <rdrammeh@gmail.com>
2017-08-22 16:53:53 -0400

    Update controllers to satisfy game logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,19 +3,30 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
+    render json: {id: @game.id}
   end
 
   def destroy
-    @game = Game.find_by(id: params[:id], user: current_user.id)
+    @game = Game.find_by(id: params[:game][:id], user: params[:game][:user])
     if @game
       @game.destroy
+    end
+  end
+
+  def update
+    @game = Game.find_by(id: params[:game][:id], user: params[:game][:user])
+    if @game
+      @game.update(score: params[:game][:score], end_time: params[:game][:end_time])
+      render json: @game
+    else
+      render json: {status: 400, errors: ["Game could not be updated", "Please reach out to our team"]}, status: 400
     end
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:score, :start_time, :end_time)
+    params.require(:game).permit(:user_id, :score, :start_time, :end_time)
   end
 
 end
